36d530df4544f11e173eb6abbe7ce44d20975b42
gap-byte <gayuhadi.prayoga@gmail.com>
2024-10-14 14:51:40 +0700

    commit

--- Python/1. Beginner/Dice/Dice.py
+++ Python/1. Beginner/Dice/Dice.py
@@ -5,5 +5,21 @@ while True:
     if user == 1:
         number = random.randint(1,6)
         print(number)
+    elif user==2:
+        number = random.randint(1,6)
+        print(number)
+    elif user==3:
+        number = random.randint(1,6)
+        print(number)
+    elif user==4:
+        number = random.randint(1,6)
+        print(number)
+    elif user==5:
+        number = random.randint(1,6)
+        print(number)
+    elif user==6:
+        number = random.randint(1,6)
+        print(number)
     else:
+        print('the dice is only 1 to 6')
         break
