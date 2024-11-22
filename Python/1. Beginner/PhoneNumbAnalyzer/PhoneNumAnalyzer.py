531ae2e67bde2284639b90bbbf118d9f3cb996a2
gap-byte <gayuhadi.prayoga@gmail.com>
2024-11-22 09:31:54 +0700

    commit

--- Python/1. Beginner/PhoneNumbAnalyzer/PhoneNumAnalyzer.py
+++ Python/1. Beginner/PhoneNumbAnalyzer/PhoneNumAnalyzer.py
@@ -5,7 +5,7 @@ from phonenumbers import carrier
 from phonenumbers import geocoder
 from phonenumbers import timezone
 
-number = "+9185XXXXXXXX"
+number = "+6281231318080"
 number = ph.parse(number)
 print(timezone.time_zones_for_number(number))
 print(carrier.name_for_number(number, "en"))
