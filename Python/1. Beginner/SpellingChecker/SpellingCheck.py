3e750de07208f5af3ead8c7da7d985206a1f4246
gap-byte <gayuhadi.prayoga@gmail.com>
2025-10-01 15:46:18 +0700

    commit

--- Python/1. Beginner/SpellingChecker/SpellingCheck.py
+++ Python/1. Beginner/SpellingChecker/SpellingCheck.py
@@ -1,11 +1,11 @@
-#pip install pyspellchecker
+#pip instal pyspellchecker
 
-from spellchecker import SpellChecker
-corrector = SpellChecker()
+#from pyspellchecker import SpellChecker
+#corrector = SpellChecker()
 
-word = input("Enter a Word : ")
-if word in corrector:
-    print("Correct")
-else:
-    correct_word = corrector.correction(word)
-    print("Correct Spelling is ", correct_word)
+#word = input("Enter a Word : ")
+#if word in corrector:
+#    print("Correct")
+#else:
+#    correct_word = corrector.correction(word)
+#    print("Correct Spelling is ", correct_word)
