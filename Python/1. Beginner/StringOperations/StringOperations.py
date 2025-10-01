54e78d76d1ed66909a9053b8a6d14ceccf36dace
gap-byte <gayuhadi.prayoga@gmail.com>
2025-10-01 15:14:22 +0700

    commit

--- Python/1. Beginner/StringOperations/StringOperations.py
+++ Python/1. Beginner/StringOperations/StringOperations.py
@@ -15,3 +15,19 @@ print(hobby.upper())
 
 # 4. Count letters in hobby
 print("Your hobby has", len(hobby), "letters.")
+
+
+# 5. Favorite color (lowercase)
+color = input("What is your favorite color? ")
+print("Your favorite color in lowercase:", color.lower())
+
+
+# 6. Check if a word starts with a specific letter
+word = input("Enter a word: ")
+print("Does your word start with 'a'?:", word.lower().startswith("a"))
+
+
+# 7. Replace part of a string
+sentence = input("Write a short sentence: ")
+new_sentence = sentence.replace("a", "@")
+print("Here is your sentence with 'a' replaced by '@':", new_sentence)
