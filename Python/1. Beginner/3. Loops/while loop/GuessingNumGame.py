dba8dfd45c644ddd1649b690973f6cd7c7ab7575
gap-byte <gayuhadi.prayoga@gmail.com>
2026-09-05 15:00:26 +0700

    commit

--- Python/1. Beginner/3. Loops/while loop/GuessingNumGame.py
+++ Python/1. Beginner/3. Loops/while loop/GuessingNumGame.py
@@ -1,8 +1,9 @@
 import random
-n = random.randrange(1,10)
+n = random.randrange(1,20)
 guess = int(input("Enter any number: "))
 
 while n!= guess:
+    print("Welcome! \nYou have to guess a number between 1 and 20")
     if guess < n:
         print("Too low")
         guess = int(input("Enter number again: "))
--- Python/1. Beginner/3. Loops/while loop/passwordChecker.py
+++ Python/1. Beginner/3. Loops/while loop/passwordChecker.py
@@ -1,6 +1,7 @@
 password = ""
 
 while password != "apple321":
+    print('wrong password! Please try again')
     password = input("Enter the password: ")
 
 print("Access granted!")
